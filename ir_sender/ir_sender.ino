fface10866774b4465e91ddc3b5560ca5521cde7
Johan Herland <johan@herland.net>
2012-09-03 02:38:16 +0200

    Rewrite ir_sender to use Timer1 in CTC mode, for much more acccurate timing

--- ir_sender/ir_sender.ino
+++ ir_sender/ir_sender.ino
@@ -1,79 +1,88 @@
 /*
  * This sketch will send out an IR command.
  *
- * See the full tutorial at http://www.ladyada.net/learn/sensors/ir.html
- * This code is public domain, please enjoy!
+ * Set up Timer1 in CTC mode, with TOP == OCR1A, and make it trigger at a rate
+ * of 2 x 38 KHz, and then toggle OC1A/PB1 (Arduino pin 9) at every trigger.
+ * This generates a 38 KHz (50% duty cycle) square wave on that pin.
+ *
+ * We then modulate that wave using the IR command timings below.
  */
-
-// Where is the LED connected?
-#define IR_LED_port PORTB
-#define IR_LED_bit PORTB5
-const unsigned int IR_LED_pin = 13;
 
 const long timings[] = { // pulse lengths [µsecs]
 	2376, 594, 1188, 594, 594, 594, 1188, 594, 594, 594, 1188, 594,
 	594, 594, 594, 594, 1188, 594, 594, 594, 594, 594, 594, 594, 594,
 	25700,
 };
-
 const size_t timings_len = sizeof(timings)/sizeof(timings[0]);
 const size_t repeats = 2; // How many times to run the above timings.
 const size_t interval = 3 * 1000; // delay between sending command [msecs]
 
-
 void setup(void)
 {
-	pinMode(IR_LED_pin, OUTPUT);
-	Serial.begin(9600);
+	Serial.begin(115200);
+
+	cli(); // Disable interrupts while setting up
+
+	// Set up:
+	//  - DDRB1 = 1 (PORTB1/OC1A in output mode)
+	DDRB |= B10; // DDRB7:0
+
+	//  - Timer1:
+	//     - COM1A1:0 = 0b10 (Clear OC1A on Compare Match (see IR_on below))
+	//     - WGM13:0 = 0b0100 (CTC mode, reset on OCR1A Match)
+	//     - CS12:0 = 0b001 (no prescaling)
+	//  - No interrupts
+	TCCR1A = B10000000; // COM1A1 COM1A0 COM1B1 COM1B0 - - WGM11 WGM10
+	TCCR1B = B00001001; // ICNC1 ICES1 - WGM13 WGM12 CS12 CS11 CS10
+	TCCR1C = B00000000; // FOC1A FOC1B - - - - - -
+	TIMSK1 = B00000000; // - - ICIE1 - - OCIE1B OCIE1A TOIE1
+
+	//  - Output Compare Register 1 A:
+	//     - OCR1A = 211 (match every 13.16 usec (== 2 x 38 KHz)
+	//  - Reset Timer/Counter1
+	OCR1A = 209;
+	TCNT1 = 0x0000;
+
+	//  - Reset interrupt flags (by writing a logical 1 into them)
+	TIFR1 = B00100111; // - - ICF1 - - OCF1B OCF1A TOV1
+
+	sei(); // Re-enable interrupts
 }
 
-
-// Send 38KHz pulses to the IR_LED_pin for the given number of µsecs
-void pulseIR(long usecs)
-{
-	while (usecs > 0) {
-		// 38 kHz is about 13 µsecs high and 13 µsecs low
-		IR_LED_port |= _BV(IR_LED_bit);
-		delayMicroseconds(13);
-		IR_LED_port &= ~_BV(IR_LED_bit);
-		delayMicroseconds(12);
-
-		// so 26 µsecs altogether
-		usecs -= 26;
-	}
-}
-
-
-// Send the IR pulses described in the timings array.
-void sendIRcode(void)
-{
-	boolean on = true;
-
-	cli(); // turn off background interrupts
-
-	for (size_t i = 0; i < timings_len; i++) {
-		if (on)
-			pulseIR(timings[i]);
-		else {
-			size_t time_left = timings[i];
-			while (time_left > 10000) {
-				delayMicroseconds(10000);
-				time_left -= 10000;
-			}
-			delayMicroseconds(time_left);
-		}
-		on = !on;
-	}
-
-	sei(); // turn on background interrupts
-}
-
+/*
+ * Modulate the IR carrier by switching between
+ *  - COM1A1:0 == 0b01 (Toggle OC1A on compare match, i.e. carrier present)
+ *  - COM1A1:0 == 0b10 (Clear OC1A on compare match, i.e. carrier absent)
+ */
+#define IR_on()  (TCCR1A = B01000000)
+#define IR_off() (TCCR1A = B10000000)
 
 void loop(void)
 {
+	unsigned long t;
+
 	Serial.println("Sending IR signal");
-	for (size_t i = 0; i < repeats; i++)
-		sendIRcode();
+	Serial.flush();
+	t = millis();
+	cli(); // turn off background interrupts
+	for (size_t r = 0; r < repeats; r++) {
+		for (size_t i = 0; i < timings_len; i++) {
+			i % 2 == 0 ? IR_on() : IR_off();
+			// Split time_left in 16 msec chunks, since
+			// delayMicroseconds() cannot handle >16.383 msec delays
+			size_t time_left = timings[i];
+			while (time_left > 16000) {
+				delayMicroseconds(16000);
+				time_left -= 16000;
+			}
+			delayMicroseconds(time_left);
+		}
+	}
+	sei(); // turn on background interrupts
+	t = millis() - t;
+	Serial.print("Sent IR signal in ");
+	Serial.print(t);
+	Serial.println(" msecs");
 
 	delay(interval);
 }
