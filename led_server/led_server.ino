befc6665ce8b40f4f2c04d72e6669d566f6c16c2
Johan Herland <johan@herland.net>
2013-01-21 22:34:23 +0100

    Rewrite led_server in terms of the new RCN_Host class

--- led_server/led_server.ino
+++ led_server/led_server.ino
@@ -1,26 +1,44 @@
 /*
- * Server for controlling 3 LEDs through RCN
+ * Server for controlling PWM pins through RCN
  *
  * Author: Johan Herland <johan@herland.net>
  * License: GNU GPL v2 or later
  */
 
 // #define DEBUG 1
+
+#if DEBUG
 #define LOG Serial.print
+#define LOGln Serial.println
+#else
+#define LOG(...)
+#define LOGln(...)
+#endif
 
-#include <RF12.h> // Needed by rcn_common.h
-#include <rcn_common.h>
+#define RCN_HOST_MAX_CHANNELS 6
 
-// Utility macros
+#include <RF12.h> // Needed by RCN
+#include <rcn_host.h>
+
 #define ARRAY_LENGTH(a) ((sizeof (a)) / (sizeof (a)[0]))
-#define LIMIT(min, val, max) (min > val ? min : (max < val) ? max : val)
 
-const byte VERSION = 3;
-RCN_Node node(RF12_868MHZ, 123, 1);
-RCN_Node::RecvPacket recvd;
-const byte pwm_pins[3] = {5, 6, 9};
+const byte VERSION = 4;
+const byte PWM_PINS[RCN_HOST_MAX_CHANNELS] = {3, 5, 6, 9, 10, 11};
 
-byte rot_values[3] = { 0 }; // R/G/B channel values
+// The following function is called whenever a channel is being updated.
+uint8_t update_filter(
+	uint8_t channel, // The channel id
+	uint8_t range, // The registered range for this channel
+	uint8_t data, // The auxiliary data for this channel
+	uint8_t old_level, // The old/current level
+	uint8_t new_level) // The proposed new level
+{
+	// Adjust PWM pin 'data' from 'old_level' to 'new_level'
+	analogWrite(data, new_level);
+	return new_level;
+}
+
+RCN_Host host(RF12_868MHZ, 123, 1, update_filter);
 
 void setup(void)
 {
@@ -28,70 +46,17 @@ void setup(void)
 	Serial.print(F("LED server version "));
 	Serial.println(VERSION);
 
-	node.init();
+	host.init();
 
-	// Initialize R/G/B LEDs to all black
-	for (int i = 0; i < ARRAY_LENGTH(pwm_pins); i++) {
-		analogWrite(pwm_pins[i], 0);
-		node.send_status_update(i, 0);
+	// Initialize one channel for each PWM pin.
+	for (int i = 0; i < ARRAY_LENGTH(PWM_PINS); i++) {
+		host.addChannel(0xff, 0, PWM_PINS[i]);
 	}
 
 	Serial.println(F("Ready"));
 }
 
-void handle_update_request(const RCN_Node::RecvPacket& p)
-{
-	if (p.channel() >= ARRAY_LENGTH(pwm_pins)) {
-		Serial.print(F("Illegal channel number: "));
-		Serial.println(p.channel());
-		return;
-	}
-
-	if (p.relative() && p.rel_level() == 0) {
-#ifdef DEBUG
-		Serial.print(F("Status request for channel #"));
-		Serial.println(p.channel());
-#endif
-	}
-	else if (p.relative()) { // relative adjustment
-		// Adjust current channel, but limit to 0 <= level <= 255
-		int l = rot_values[p.channel()] + p.rel_level();
-		l = LIMIT(0x00, l, 0xff);
-#ifdef DEBUG
-		Serial.print(F("Adjusting channel #"));
-		Serial.print(p.channel());
-		Serial.print(F(": "));
-		Serial.print(rot_values[p.channel()]);
-		Serial.print(F(" + "));
-		Serial.print(p.rel_level());
-		Serial.print(F(" => "));
-		Serial.println(l);
-#endif
-		rot_values[p.channel()] = l;
-	}
-	else { // absolute assignment
-		int l = LIMIT(0x00, p.abs_level(), 0xff);
-#ifdef DEBUG
-		Serial.print(F("Setting channel #"));
-		Serial.print(p.channel());
-		Serial.print(F(": "));
-		Serial.print(rot_values[p.channel()]);
-		Serial.print(F(" -> "));
-		Serial.print(p.abs_level());
-		Serial.print(F(" => "));
-		Serial.println(l);
-#endif
-		rot_values[p.channel()] = l;
-	}
-
-	// Refresh corresponding LED
-	analogWrite(pwm_pins[p.channel()], rot_values[p.channel()]);
-	// Send status update as reply
-	node.send_status_update(p.channel(), rot_values[p.channel()]);
-}
-
 void loop(void)
 {
-	if (node.send_and_recv(recvd))
-		handle_update_request(recvd);
+	host.run();
 }
