3454af1efda04b550ee2fa5badfc329fd37f56d5
Johan Herland <johan@herland.net>
2013-01-21 22:52:50 +0100

    Teach LED server to advertise and control all (PWM and non-PWM) pins from 3-9

--- led_server/led_server.ino
+++ led_server/led_server.ino
@@ -1,5 +1,5 @@
 /*
- * Server for controlling PWM pins through RCN
+ * Server for controlling PWM and non-PWN pins connected through RCN
  *
  * Author: Johan Herland <johan@herland.net>
  * License: GNU GPL v2 or later
@@ -15,7 +15,7 @@
 #define LOGln(...)
 #endif
 
-#define RCN_HOST_MAX_CHANNELS 6
+#define RCN_HOST_MAX_CHANNELS 7
 
 #include <RF12.h> // Needed by RCN
 #include <rcn_host.h>
@@ -23,7 +23,8 @@
 #define ARRAY_LENGTH(a) ((sizeof (a)) / (sizeof (a)[0]))
 
 const byte VERSION = 4;
-const byte PWM_PINS[RCN_HOST_MAX_CHANNELS] = {3, 5, 6, 9, 10, 11};
+const byte   PINS[RCN_HOST_MAX_CHANNELS] = { 3,  4,  5,   6,  7, 8,  9 };
+const byte RANGES[RCN_HOST_MAX_CHANNELS] = {255, 1, 255, 255, 1, 1, 255};
 
 // The following function is called whenever a channel is being updated.
 uint8_t update_filter(
@@ -33,8 +34,11 @@ uint8_t update_filter(
 	uint8_t old_level, // The old/current level
 	uint8_t new_level) // The proposed new level
 {
-	// Adjust PWM pin 'data' from 'old_level' to 'new_level'
-	analogWrite(data, new_level);
+	// Adjust pin number 'data' from 'old_level' to 'new_level'
+	if (range == 1) // on/off pin
+		digitalWrite(data, new_level);
+	else // PWM pin
+		analogWrite(data, new_level);
 	return new_level;
 }
 
@@ -49,9 +53,10 @@ void setup(void)
 #endif
 	host.init();
 
-	// Initialize one channel for each PWM pin.
-	for (int i = 0; i < ARRAY_LENGTH(PWM_PINS); i++) {
-		host.addChannel(0xff, 0, PWM_PINS[i]);
+	// Initialize one channel for each pin.
+	for (int i = 0; i < ARRAY_LENGTH(PINS); i++) {
+		pinMode(PINS[i], OUTPUT);
+		host.addChannel(RANGES[i], 0, PINS[i]);
 	}
 
 #if DEBUG
