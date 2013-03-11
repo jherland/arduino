279c194b4dc1c47e8f850aefa1c58c53100956f7
Johan Herland <johan@herland.net>
2013-03-11 03:15:58 +0100

    Rewrite nexa_decoder to handle both old and new Nexa formats

--- nexa_decoder/nexa_decoder.ino
+++ nexa_decoder/nexa_decoder.ino
@@ -33,22 +33,28 @@
 #define RF_SETUP() bitClear(DDRB, 0)
 #define RF_READ()  bitRead(PINB, 0)
 
-#define ARRAY_LENGTH(a) ((sizeof (a)) / (sizeof (a)[0]))
+const unsigned int BUF_SIZE = 1536;
+char buf[BUF_SIZE];
+size_t i = 0;
 
-enum PulseType {
-	PULSE_A,  // ~10 150µs LOW pulse that starts the sync
-	PULSE_B,  // ~2 643µs LOW pulse that continues the sync
-	PULSE_X,  // ~1 236µs LOW pulse that is a half of a bit
-	PULSE_Y,  // ~215µs LOW pulse that is the other half of a bit
-	PULSE_H,  // ~310µs HIGH pulse
-	PULSE_NO  // Not one of the others. An invalid pulse.
-};
+enum {
+	UNKNOWN, SX1, SX2, SX3,
+	DA0, DA1, DA2, DA3,
+	DB0, DB1, DB2, DB3,
+} cur_state = UNKNOWN, new_state;
+
+byte cur_bit = 0;
 
 void setup()
 {
+	/*
+	 * TODO: Connect RF input to ICP1/PB0 (Arduino pin #8), and set up
+	 * an interrupt to be triggered and a timestamp recorded on every
+	 * pin change.
+	 */
 	RF_SETUP();
 	Serial.begin(115200);
-	Serial.println(F("rf_decoder ready:"));
+	Serial.println(F("nexa_decoder ready:"));
 }
 
 /*
@@ -87,84 +93,152 @@ int next_pulse()
 }
 
 /*
- * Convert value from next_pulse() to one of the expected pulse types.
- */
-/* enum PulseType */ int quantize_pulse(int p)
-{
-	// comments: min/max from observations
-	if (p > 0 && p <= 1000) // 248 / 432
-		return PULSE_H;
-	else if (p < 0 && p >= -500) // -44 / -284
-		return PULSE_Y;
-	else if (p <= -500 && p >= -1500) // -1132 / -1300
-		return PULSE_X;
-	else if (p <= -2000 && p >= -3000) // -2592 / -2692
-		return PULSE_B;
-	else if (p <= -9000 && p >= -11000) // -10080 / -10208
-		return PULSE_A;
-	else
-		return PULSE_NO;
-}
-
-/*
- * Look for the expected RF sync pattern, and return when detected.
- */
-void detect_sync()
-{
-	// Sync pattern: A-H-B-H
-	static const int sync[] = { PULSE_A, PULSE_H, PULSE_B, PULSE_H };
-	unsigned int i = 0;
-	while (i < ARRAY_LENGTH(sync))
-		i = quantize_pulse(next_pulse()) == sync[i] ? i + 1 : 0;
-}
-
-/*
- * Decode a data bit from the RF receiver.
+ * Return number of bits needed to represent the given number.
  *
- * Return true if a data bit was successfully decoded and saved into bit,
- * false if no data bit was decoded and the current transmission is not a
- * valid command.
+ * This is equivalent to floor(log2(v)) + 1.
  */
-bool read_data_bit(int& bit)
+unsigned int numbits(unsigned int v)
 {
-	/*
-	 * Decode Y-H-X-H into '0' and X-H-Y-H into '1'. Everything else
-	 * is invalid data.
-	 */
-	int first_pulse = quantize_pulse(next_pulse());
-	if (!(first_pulse == PULSE_X || first_pulse == PULSE_Y))
-		return false;
-	if (quantize_pulse(next_pulse()) != PULSE_H)
-		return false;
-	int second_pulse = quantize_pulse(next_pulse());
-	if (first_pulse + second_pulse != PULSE_X + PULSE_Y)
-		return false;
-	if (quantize_pulse(next_pulse()) != PULSE_H)
-		return false;
-	bit = first_pulse == PULSE_X;
-	return true;
+	unsigned int ret = 0;
+	while (v) {
+		v >>= 1;
+		ret++;
+	}
+	return ret;
+}
+
+/*
+ * Classify the given pulse length into the following categories:
+ *
+ * 0: Invalid pulse length (i.e. not within any of the below categories)
+ * 1:    0µs <= p <   512µs
+ * 2:  512µs <= p <  2048µs
+ * 3: 2048µs <= p <  4096µs
+ * 4: 4096µs <= p <  8192µs
+ * 5: 8192µs <= p < 16384µs
+ *
+ * The above categories (1..5) are positive (1..5) for HIGH pulses, and
+ * negative (-1..-5) for LOW pulses.
+ */
+int quantize_pulse(int p)
+{
+	int sign = (p > 0) ? 1 : -1;
+	p *= sign; // abs value
+	p >>= 9; // divide by 512
+	switch (numbits(p)) {
+		case 0: // 0µs <= p < 512µs
+			return sign * 1;
+		case 1: // 512µs <= p < 1024µs
+		case 2: // 1024µs <= p < 2048µs
+			return sign * 2;
+		case 3: // 2048µs <= p < 4096µs
+			return sign * 3;
+		case 4: // 4096µs <= p < 8192µs
+			return sign * 4;
+		case 5: // 8192µs <= p < 16384µs
+			return sign * 5;
+		default:
+			return 0;
+	}
 }
 
 void loop()
 {
-	const unsigned int BUF_SIZE = 40;
-	char buf[BUF_SIZE];
-	unsigned int i = 0;
-
-	detect_sync();
-	buf[i++] = '>';
-	for (i = 0; i < 32; i++) {
-		int b;
-		if (!read_data_bit(b)) { // failed to complete command
-			buf[i++] = '\0';
-			Serial.println(buf);
-			Serial.println(F("and then FAIL"));
-			Serial.flush();
-			return;
-		}
-		buf[i++] = b ? '1' : '0';
+	new_state = UNKNOWN;
+	int p = quantize_pulse(next_pulse()); // current pulse
+	switch (p) {
+		case -5: // LOW: 8192µs <= pulse < 16384µs
+			new_state = SX1;
+			break;
+		case -3: // LOW: 2048µs <= pulse < 4096µs
+			if (cur_state == SX2) // cmd format A
+				new_state = SX3;
+			break;
+		case -2: // LOW: 512µs <= pulse < 2048µs
+			if (cur_state == DA0) {
+				cur_bit = '1';
+				new_state = DA1;
+			}
+			else if (cur_state == DA2 && cur_bit == '0')
+				new_state = DA3;
+			else if (cur_state == SX2 || cur_state == DB1) {
+				if (cur_state == SX2) { // cmd format B
+					buf[i++] = '\n';
+					buf[i++] = 'B';
+				}
+				new_state = DB2;
+			}
+			else if (cur_state == DB3 && cur_bit == '0') {
+				buf[i++] = cur_bit;
+				cur_bit = 0;
+				new_state = DB0;
+			}
+			break;
+		case -1: // LOW: 0µs <= pulse < 512µs
+			if (cur_state == DA0) {
+				cur_bit = '0';
+				new_state = DA1;
+			}
+			else if (cur_state == DA2 && cur_bit == '1')
+				new_state = DA3;
+			else if (cur_state == DB3 && cur_bit == '1') {
+				buf[i++] = cur_bit;
+				cur_bit = 0;
+				new_state = DB0;
+			}
+			break;
+		case 2: // HIGH: 512µs <= pulse < 2048µs
+			if (cur_state == DB2) {
+				cur_bit = '1';
+				new_state = DB3;
+			}
+			break;
+		case 1: // HIGH: 0µs <= pulse < 512µs
+			switch (cur_state) {
+				case SX1:
+					new_state = SX2;
+					break;
+				case SX3:
+					buf[i++] = '\n';
+					buf[i++] = 'A';
+					new_state = DA0;
+					break;
+				case DA1:
+					new_state = DA2;
+					break;
+				case DA3:
+					buf[i++] = cur_bit;
+					cur_bit = 0;
+					new_state = DA0;
+					break;
+				case DB0:
+					new_state = DB1;
+					break;
+				case DB2:
+					cur_bit = '0';
+					new_state = DB3;
+					break;
+			}
+			break;
 	}
-	buf[i++] = '\0';
-	Serial.println(buf);
-	Serial.flush();
+	if (i >= BUF_SIZE - 5) { // Prevent overflowing buf
+		buf[i++] = 'X';
+		new_state = UNKNOWN;
+	}
+	if (cur_state != UNKNOWN && new_state == UNKNOWN) { // => UNKNOWN
+		// Invalid data: Print buffer.
+		// ...but only if it has > 8 valid bits
+		if (i > 8 + 2) {
+			buf[i++] = '\0';
+			Serial.print(buf);
+			Serial.print('>');
+			Serial.print(cur_state);
+			Serial.print('|');
+			Serial.print(p);
+			Serial.print('<');
+			Serial.flush();
+		}
+		i = 0; // Restart buffer
+	}
+	cur_state = new_state;
 }
