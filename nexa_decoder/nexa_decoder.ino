9ff18fd5a7f5c79d3dbc5e8275363e3c5c3f9da6
Johan Herland <johan@herland.net>
2013-03-29 02:17:49 +0100

    Move parsing of bit sequences into NexaCommand

--- nexa_decoder/nexa_decoder.ino
+++ nexa_decoder/nexa_decoder.ino
@@ -57,108 +57,6 @@ void setup()
 	Serial.println(F("nexa_decoder ready:"));
 }
 
-/*
- * Add the given bit into the given byte array at the given bit index.
- *
- * The bit index is LSB, so index 0 corresponds to the LSB of
- * dst[dst_len - 1], index 7 corresponds to the MSB of dst[dst_len - 1],
- * index 8 corresponds to the LSB of dst[dst_len - 2], and so on.
- */
-void add_bit(byte * dst, size_t dst_len, size_t bit_idx, int bit_val)
-{
-	// assert(bit_idx < 8 * dst_len);
-	size_t byte_idx = dst_len - (1 + bit_idx / 8);
-	byte bit_mask = 1 << (bit_idx % 8);
-	if (bit_val)
-		dst[byte_idx] |= bit_mask;
-	else
-		dst[byte_idx] &= ~bit_mask;
-}
-
-/*
- * Initialize the given old-style NexaCommand from the 12 bits at buf.
- *
- * The command bits are of the form: DDDDDDDD011S
- */
-void parse_12bit_cmd(NexaCommand & cmd, const char buf[12])
-{
-	cmd.version = NexaCommand::NEXA_12BIT;
-	cmd.device[0] = 0;
-	cmd.device[1] = 0;
-	for (size_t i = 0; i < 8; ++i)
-		add_bit(cmd.device + 2, 1, i, buf[i] == '1');
-	cmd.channel = 0;
-	cmd.group = 0;
-	cmd.state = buf[11] == '1';
-}
-
-/*
- * Initialize the given new-style NexaCommandfrom the 32 bits at buf.
- *
- * The command bits are of the form: DDDDDDDDDDDDDDDDDDDDDDDD10GSCCCC
- */
-void parse_32bit_cmd(NexaCommand & cmd, const char buf[32])
-{
-	size_t i;
-	cmd.version = NexaCommand::NEXA_32BIT;
-	for (i = 0; i < 24; ++i)
-		add_bit(cmd.device, ARRAY_LENGTH(cmd.device), i,
-			buf[i] == '1');
-	cmd.channel = (buf[28] == '1' ? B1000 : 0) |
-	              (buf[29] == '1' ? B100 : 0) |
-	              (buf[30] == '1' ? B10 : 0) |
-	              (buf[31] == '1' ? B1 : 0);
-	cmd.group = buf[26] == '1';
-	cmd.state = buf[27] == '1';
-}
-
-/*
- * Parse data from ring buffer, and generate NexaCommands.
- *
- * The function will consume (some, but not necessarily all) available
- * data in the ring buffer.
- *
- * This function will not generate a NexaCommand every time it's called,
- * but when it does, it will return true, and store the NexaCommand into
- * the given object.
- */
-bool decode_bits(NexaCommand & cmd, RingBuffer<char> & rx_bits)
-{
-	static NexaCommand::Version version = NexaCommand::NEXA_INVAL;
-	static char buf[32]; // Long enough for the longest command
-	static size_t buf_pos = 0;
-	static size_t expect = 0;
-	while (!rx_bits.r_empty()) {
-		char b = rx_bits.r_pop();
-		if (b == 'A' || b == 'B') {
-			buf_pos = 0;
-			if (b == 'A') {
-				version = NexaCommand::NEXA_32BIT;
-				expect = 32;
-			}
-			else {
-				version = NexaCommand::NEXA_12BIT;
-				expect = 12;
-			}
-		}
-		else if ((b == '0' || b == '1') && buf_pos < expect)
-			buf[buf_pos++] = b;
-
-		if (expect && buf_pos == expect) { // all bits present
-			if (version == NexaCommand::NEXA_12BIT)
-				parse_12bit_cmd(cmd, buf);
-			else if (version == NexaCommand::NEXA_32BIT)
-				parse_32bit_cmd(cmd, buf);
-
-			expect = 0;
-			buf_pos = 0;
-			version = NexaCommand::NEXA_INVAL;
-			return true;
-		}
-	}
-	return false;
-}
-
 void loop()
 {
 	pulse_parser(rf_port.rx_get_pulse());
@@ -169,7 +67,7 @@ void loop()
 		Serial.write((const byte *) rx_bits.r_wrapped_buf(),
 			     rx_bits.r_wrapped_buf_len());
 #endif
-		if (decode_bits(cmd, rx_bits)) {
+		if (NexaCommand::from_bit_buffer(cmd, rx_bits)) {
 #if DEBUG
 			Serial.println();
 #endif
